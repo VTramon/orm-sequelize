6608a88d9731ff17991fdce598f26a1de6adda48
VTramon <vitorr.ooliveira@gmail.com>
2021-09-28 22:24:49 -0300

    first commit

---
+++ api/migrations/20210925185641-addcolumn-turmas.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Turmas', 'deletedAt', {
+        allowNull: true,
+        type: Sequelize.DATE,
+    })
+},
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Turmas', 'deletedAt');
+  },
+}
---
+++ api/routes/niveisRoute.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const NivelController = require('../controllers/NivelController');
+
+const router = Router();
+
+router.get('/niveis', NivelController.pegaTodosOsNiveis);
+router.get('/niveis/:id', NivelController.pegaNivel);
+router.post('/niveis', NivelController.criaNivel);
+router.put('/niveis/:id', NivelController.atualizaNivel);
+router.delete('/niveis/:id', NivelController.apagaNivel);
+router.post('/niveis/:id/restaura', NivelController.restauraNivel);
+
+module.exports = router;
